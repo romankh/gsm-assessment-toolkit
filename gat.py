cf193430d0261bebf42368d91a49df7d4d68639a
Roman Khassraf <roman@khassraf.at>
2017-01-27 20:34:43 +0100

    Hello world

---
+++ gat.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+import argparse
+import importlib
+import os
+
+from gat.core.common.config import ConfigProvider
+
+if __name__ == '__main__':
+    """
+    Instantiate and start the console.
+    """
+
+    """
+    Install:
+        - argcomplete per deb
+        - pip install requests
+    """
+    # change to base directory
+    filepath = os.path.realpath(__file__)
+    basedir = os.path.dirname(filepath)
+    os.chdir(basedir)
+
+    # load config
+    conf = ConfigProvider()
+
+    parser = argparse.ArgumentParser(description='GSM Assessment Toolkit')
+    parser.add_argument('--ui-class', action="store", dest="ui_class",
+                        help="Override the UI class configured in config file.")
+    args = parser.parse_args()
+
+    ui_clazz_full = args.ui_class
+    if ui_clazz_full is None:
+        ui_clazz_full = conf.get("gat", "ui_class")
+
+    # get the configured UI class
+    ui_clazz_module, ui_clazz_name = ui_clazz_full.rsplit(".", 1)
+    try:
+        ui_clazz = getattr(importlib.import_module(ui_clazz_module), ui_clazz_name)
+    except:
+        print "Failed to load UI."
+        exit(1)
+
+    # instantiate UI class and start it.
+    app = ui_clazz(basedir, conf)
+    app.cmdloop()

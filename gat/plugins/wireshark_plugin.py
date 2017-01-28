7812534ba2c97ac696581c088a312e95904fabe5
Roman Khassraf <roman@khassraf.at>
2017-01-28 09:27:08 +0100

    Adds wireshark launcher plugin

---
+++ gat/plugins/wireshark_plugin.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+import subprocess
+
+from gat.core.plugin.interface import plugin, PluginBase, cmd
+
+
+@plugin(name="Wireshark", description="Wireshark related commands.")
+class WiresharkPlugin(PluginBase):
+    @cmd(name="wireshark", description="Launch wireshark and start sniffing.")
+    def wireshark(self, args):
+        subprocess.Popen(["wireshark", "-i", "lo", "-f", "udp port 4729 && !icmp", "-k"])

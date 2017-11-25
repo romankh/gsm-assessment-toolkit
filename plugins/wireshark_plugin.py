757b801bfa27df1ad9e3a87516a235fd1aeef270
Roman Khassraf <roman@khassraf.at>
2017-11-25 09:15:14 +0100

    Fix output from wireshark

--- plugins/wireshark_plugin.py
+++ plugins/wireshark_plugin.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 import subprocess
+import os
 
 from core.plugin.interface import plugin, PluginBase, cmd
 
@@ -8,4 +9,6 @@ from core.plugin.interface import plugin, PluginBase, cmd
 class WiresharkPlugin(PluginBase):
     @cmd(name="wireshark", description="Launch wireshark and start sniffing.")
     def wireshark(self, args):
-        subprocess.Popen(["wireshark", "-i", "lo", "-f", "udp port 4729 && !icmp", "-k"])
+        FNULL = open(os.devnull, 'w')
+        subprocess.Popen(["wireshark", "-i", "lo", "-f", "udp port 4729 && !icmp", "-k"],
+                         stdout=FNULL, stderr=subprocess.STDOUT)
